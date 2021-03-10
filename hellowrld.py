039a754fcda9e45a4373e9dfe8f0cf094264e36a
Life2death <47413086+Life2death@users.noreply.github.com>
2021-03-10 18:35:22 +0530

    TEWT
    TEST

---
+++ hellowrld.py
@@ -0,0 +1,15 @@
+""" name = 'Vikram Panmand'
+email = f"""
+hello {name}
+wtf are u trying to do 
+"""
+#print (name.upper())
+#print(len(name))
+print(email) """
+""" person = input( 'Whats your name')
+print ('Hello',person,'how are you') """
+while true:
+num = int(input("Enter the number:"))
+print("multiplication table of ",num)
+for i in range(1,11):
+    print(num,"X",i,"=",num * i)
